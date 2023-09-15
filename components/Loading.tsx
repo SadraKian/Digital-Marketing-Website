90102df4fe94ca91c3855a4ccd1b4fb0affd8d24
SadraKian <sadrakiandeveloper@gmail.com>
2023-09-15 19:23:22 +0330

    Modifying loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,7 +5,20 @@ import { useContext } from "react";
 
 const Loading = () => {
   const { isThemeDark } = useContext(themeContext);
-  return <div></div>;
+  const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  return (
+    <section className="relative w-screen h-screen ">
+      {numbers.map((number) => (
+        <span
+          className={`absolute top-0 left-0 rotate-[${
+            180 * number
+          }] before:content-[""] before:w-4 before:h-4 rounded-[50% ] before:absolute before:top-0 before:left-0 span-shadow`}
+        ></span>
+      ))}
+    </section>
+  );
 };
 
 export default Loading;
