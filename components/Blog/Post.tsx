b5a299ca4988a2e631beb32f4a295439fc16f276
SadraKian <sadrakiandeveloper@gmail.com>
2023-10-14 01:16:33 +0330

    updating blog posts

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -81,7 +81,7 @@ const Post = () => {
           <p
             className={`${
               !isThemeDark ? "text-black" : "text-white"
-            }w-[90%] sm:w-[85%] lg:w-[80%] 2xl:ml-16 text-white sm:text-[18px] lg:text-[20px] leading-8 md:leading-9 font-semibold space tracking-wide`}
+            }w-[90%] sm:w-[85%] lg:w-[80%] 2xl:ml-16  sm:text-[18px] lg:text-[20px] leading-8 md:leading-9 font-semibold space tracking-wide`}
           >
             {postData.text}
           </p>
